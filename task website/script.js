882c14b124dde63d3a77704a5ec2f9e6637a919a
ashrv12 <151522619+ashrv12@users.noreply.github.com>
2023-12-26 19:33:19 +0800

    update

---
+++ task website/script.js
@@ -0,0 +1,16 @@
+// Use createElement to create a new element
+
+let newDiv= document.createElement('div')
+
+newDiv.innerHTML = '<p>You mom gay</p>'
+
+document.body.appendChild(newDiv)
+
+
+// Use appendChild to append a node to a list of child nodes
+
+// Use textContent to get and set the text content of a node
+
+// Use innerHTML to get and set the HTML content of an element
+
+// Compare innerHTML vs. createElement for creating new elements
