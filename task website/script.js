be802de7f3df617c39692250272b9e15d6a958ba
ashrv12 <151522619+ashrv12@users.noreply.github.com>
2023-12-27 17:29:46 +0800

    update with the new task card

--- task website/script.js
+++ task website/script.js
@@ -1,16 +1,11 @@
-// Use createElement to create a new element
+function showNewTask() {
+    const newTask = document.getElementById("create-task");
 
-let newDiv= document.createElement('div')
+    newTask.style.display = 'flex';
+}
 
-newDiv.innerHTML = '<p>You mom gay</p>'
+function exitcard() {
+    const exitTask = document.getElementById("create-task");
 
-document.body.appendChild(newDiv)
-
-
-// Use appendChild to append a node to a list of child nodes
-
-// Use textContent to get and set the text content of a node
-
-// Use innerHTML to get and set the HTML content of an element
-
-// Compare innerHTML vs. createElement for creating new elements
+    exitTask.style.display = 'none';
+}
