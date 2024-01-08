3e20eb21e7271265375bc964fbf05e321c5f6ea9
ashrv12 <151522619+ashrv12@users.noreply.github.com>
2024-01-08 19:26:46 +0800

    swiper no swiping

--- task website/script.js
+++ task website/script.js
@@ -38,6 +38,53 @@ function showEditCard() {
 
   editCard.style.display = "flex";
 }
+
+function confirmEdit() {
+  const editTitle = document.getElementById("edit-title").value;
+  const editDescription = document.getElementById("edit-desc").value;
+  const editCategor = document.getElementById("edit-categor").value;
+  const editStatus = document.getElementById("edit-impo").value;
+
+  generateId++;
+
+  sortedMain.push({
+    id: generateId,
+    title: editTitle,
+    description: editDescription,
+    category: editCategor,
+    priority: editStatus,
+  });
+
+  const order = { high: 1, medium: 2, low: 3 };
+
+  sortedMain.sort((a, b) => {
+    return order[a.priority] - order[b.priority];
+  });
+
+  document.getElementById("todo-target").innerHTML = "";
+  document.getElementById("inprogress-target").innerHTML = "";
+  document.getElementById("stuck-target").innerHTML = "";
+  document.getElementById("giveup-target").innerHTML = "";
+
+  for (x = 0; x < sortedMain.length; x++) {
+    // we first need to know which category it belongs to
+
+    currentTarget = categoryFind(sortedMain[x].category);
+
+    //   writediv(priorityFind(sortedMain[x].priority));
+
+    renderdiv(x);
+
+    resetEdit();
+    exiteditcard();
+    count();
+  }
+}
+
+function resetEdit() {
+  document.getElementById("edit-desc").value = "";
+  document.getElementById("edit-title").value = "";
+}
 // edit card close
 
 // the counting function
@@ -150,7 +197,7 @@ function renderdiv(index) {
         </div>
         <div class="icondiv">
             <svg onclick="removecard(${index})" class="content-icon" xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path opacity="1" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
-            <svg onclick="showEditCard(${index})" class="edit-button" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path opacity="1" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
+            <svg onclick="showEditCard(${index}), removecard(${index})" class="edit-button" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path opacity="1" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
         </div>
     </div>`;
   const todoElement = document.getElementById(currentTarget);
@@ -158,6 +205,7 @@ function renderdiv(index) {
   todoElement.innerHTML = todoElement.innerHTML + tasthtml;
   console.log({ tasthtml });
 }
+
 
 // resetting the input card fields
 
@@ -198,14 +246,16 @@ function priorityFind(prio) {
 
 function removecard(index) {
   count()
+
   sortedMain.splice(index, 1);
+
   console.log("remove: ", JSON.stringify(sortedMain, null, 2));
+
   document.getElementById("todo-target").innerHTML = "";
   document.getElementById("inprogress-target").innerHTML = "";
-
   document.getElementById("stuck-target").innerHTML = "";
-
   document.getElementById("giveup-target").innerHTML = "";
+
   for (x = 0; x < sortedMain.length; x++) {
     // we first need to know which category it belongs to
 
