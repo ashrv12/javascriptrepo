db7a6c7cfa92d5ea6ffc60652fb8ea112cdb4bc1
ashrv12 <151522619+ashrv12@users.noreply.github.com>
2024-01-11 19:00:41 +0800

    asdf

--- rockpaper.js
+++ rockpaper.js
@@ -1,5 +1,8 @@
 const playerChoiceElement = document.querySelector("#playerChoise");
 const computerChoiceElement = document.querySelector("#computerChoise");
+const buttonArray = document.querySelectorAll(".move")
+
+console.log(buttonArray)
 
 let playerScore = 0;
 let computerScore = 0;
@@ -15,25 +18,49 @@ function playgame(move) {
   playerChoiceElement.innerText = move;
   computerChoiceElement.innerText = computerChoice;
 
+  if (move === computerChoice) {
+    console.log("draw");
+    return;
+  }
+
+
   if (
-    ("rock" == move && "scissors" == computerChoice) ||
-    ("paper" == move && "rock" == computerChoice) ||
-    ("scissors" == move && "paper" == computerChoice)
+    ("rock" === move && "scissors" === computerChoice) ||
+    ("paper" === move && "rock" === computerChoice) ||
+    ("scissors" === move && "paper" === computerChoice)
   ) {
     playerScore++;
-  }
-  else if (move == computerChoice) {
-    console.log("draw");
   } else {
     computerScore++;
   }
-  document.querySelector("#score").innerHTML = "";
-  document.querySelector("#score").innerHTML = `Player: ${playerScore} | Computer: ${computerScore}`;
+  document.querySelector("#score").innerText = "";
+  document.querySelector("#score").innerText = `Player: ${playerScore} | Computer: ${computerScore}`;
+
+  checkEnd(playerScore);
+  console.log("current player score is ",playerScore)
+  checkEnd(computerScore);
+  console.log("current computer score is ",computerScore)
 }
+
+function disable(button) {
+  button.disabled = true;
+}
+
+function enable(button) {
+  button.disabled = false;
+}
+
 
 function reset() {
   document.querySelector("#score").innerHTML = "";
   document.querySelector("#score").innerHTML = `Player: 0 | Computer: 0`;
   playerScore = 0;
   computerScore = 0;
+  buttonArray.forEach(enable)
+}
+
+function checkEnd (input) {
+  if (input === 3) {
+    buttonArray.forEach(disable)
+  }
 }
